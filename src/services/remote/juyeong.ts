25e7941d7bef3818f464a251fcdf1c72e1d2d1c2
jynam <njy1007@gmail.com>
2022-05-22 03:36:45 +0900

    fix: 머 고침

--- src/services/remote/juyeong.ts
+++ src/services/remote/juyeong.ts
@@ -1,11 +1,11 @@
+import { JuyeongService } from '../api/juyeong';
 import { GroupInfo } from '../api/types/juyeong';
 import { API } from './base';
 
-export function juyeongRemote(): any {
+export function juyeongRemote(): JuyeongService {
   const createGroup = async (groupInfo: GroupInfo) => {
-    const code = await API.post({ url: '/team', data: groupInfo });
-    // const code = await axios.post('/team', groupInfo);
-    return code;
+    const response = await API.post({ url: '/team', data: groupInfo });
+    return response.data.inviteCode;
   };
 
   return {
