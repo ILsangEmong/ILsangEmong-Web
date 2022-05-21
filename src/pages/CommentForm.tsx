9905b316997b141f0203f3d688956e041c6c3e4b
songah119 <skutcm119@gmail.com>
2022-05-22 04:07:27 +0900

    feat: 와프 기반 화면 구현

--- src/pages/CommentForm.tsx
+++ src/pages/CommentForm.tsx
@@ -1,50 +1,131 @@
+import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export default function CommentForm() {
-  return (
-    <>
-      <StTitle>몽몽이들</StTitle>
+  // // 리뷰 텍스트 핸들링
+  // const handleTextChange = (e) => {
+  //   if (e.target.value) {
+  //     handleReviewText(e.target.value);
+  //   }
+  // };
 
-      <StContainer></StContainer>
+  return (
+    <StCommentForm>
+      <StTitle>GROUP NAME: 몽몽이들</StTitle>
+
+      <StContainer>
+        <label>Q. 오늘의 일상 속 기억하고 싶은 일은 무엇이었나요?</label>
+        <StInputReviewText
+          placeholder="50자 이내로 내용을 적어주세요."
+          maxLength={50}
+          // onChange={handleTextChange}
+        />
+      </StContainer>
 
       <Link to={'/group'}>
         <StBtn>작성완료</StBtn>
       </Link>
-    </>
+    </StCommentForm>
   );
 }
-
+const StCommentForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 const StTitle = styled.div`
-  // width: ;
-  // height: ;
+  width: 571px;
+  height: 80px;
 
   display: flex;
   justify-content: center;
+  align-items: center;
 
-  // margin-bottom :3rem;
+  box-sizing: border-box;
+
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 20px;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 38px;
+
+  margin-top: 40px;
+  margin-bottom: 211px;
 `;
 
 const StContainer = styled.div`
-  // width: ;
-  // height: ;
+  & > label {
+    width: 653px;
+    height: 38px;
+
+    font-style: normal;
+    font-weight: 700;
+    font-size: 32px;
+    line-height: 38px;
+    color: #000000;
+
+    margin-bottom: 60px;
+  }
 
   display: flex;
-  flex-direction: row;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-  // gap: 3rem;
+const StInputReviewText = styled.textarea`
+  width: 868px;
+  height: 222px;
+
+  padding: 40px;
+  margin-bottom: 259px;
+
+  background: #ffffff;
+
+  border: 2px solid #000000;
+  border-radius: 10px;
+
+  outline: none;
+  resize: none;
+
+  &::placeholder {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 29px;
+
+    letter-spacing: -0.01em;
+
+    color: #b1b1b1;
+  }
 `;
 
 const StBtn = styled.button`
-  // width: ;
-  // height: ;
+  width: 387px;
+  height: 80px;
 
   display: flex;
   justify-content: center;
+  align-items: center;
 
-  // &:hover{
-  //   background-color : skyblue;
-  //   color : blue
-  // }
+  background: #ffffff;
+
+  border: 2px solid #000000;
+  box-shadow: 10px 10px 0px #000000;
+  border-radius: 20px;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 38px;
+  text-align: center;
+
+  color: #000000;
+
+  margin-bottom: 90px;
 `;
