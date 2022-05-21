954192fb4e106fb5d7bdd3c11f4049e638862eff
songah119 <skutcm119@gmail.com>
2022-05-22 00:51:13 +0900

    chore: 라우트 마저작성

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,9 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CommentForm from '../pages/CommentForm';
+import Expire from '../pages/Expire';
+import GroupMain from '../pages/GroupMain';
+import Result from '../pages/Result';
+import ResultIntro from '../pages/ResultIntro';
+import TeamForm from '../pages/TeamForm';
 
 export default function Router() {
   return (
     <BrowserRouter>
-      {/* <Routes>
+      <Routes>
         <Route path="/create" element={<TeamForm />} />
         <Route path="/group/*" element={<GroupMain />}>
           <Route path="comment" element={<CommentForm />} />
@@ -13,7 +19,7 @@ export default function Router() {
           <Route path="*" element={<div>몽몽이들 미안! 404 에러야!</div>} />
         </Route>
         <Route path="*" element={<div>몽몽이들 미안! 404 에러야!</div>} />
-      </Routes> */}
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/CommentForm.tsx
@@ -0,0 +1,3 @@
+export default function CommentForm() {
+  return <div>CommentForm</div>;
+}
